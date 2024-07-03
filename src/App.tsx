534900a8fa7354c0c1594d643a90dd6e4d7c446b
koog7 <k.ognev709@mail.com>
2024-07-03 14:08:00 +0600

    Добавлены навбар с роутами на главной страницеÑ

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import './App.css'
-
+import {NavLink, Route, Routes} from "react-router-dom";
+import Home from "./containers/Home.tsx";
 const App = () => {
 
     return(
         <>
-
+            <div>
+                <div className="navbar">
+                    <h2><NavLink className={'logo'} to="/">Static pages</NavLink></h2>
+                    <div>
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                        <NavLink className="nav-link" to="/pages/about">About</NavLink>
+                        <NavLink className="nav-link" to="/pages/contacts">Contacts</NavLink>
+                        <NavLink className="nav-link" to="/pages/faq">FAQ</NavLink>
+                        <NavLink className="nav-link" to="/pages/services">Services</NavLink>
+                        <NavLink className="nav-link" to="/pages/admin">Admin Panel</NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route path="/" element={(
+                        <Home />
+                    )}/>
+                    <Route path="/pages/:pageName" element={(
+                        <Home />
+                    )}/>
+                </Routes>
+            </div>
         </>
     );
 
