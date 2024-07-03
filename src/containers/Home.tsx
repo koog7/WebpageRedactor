730afcc687d7bd9304344ab2d33d68b023494864
koog7 <k.ognev709@mail.com>
2024-07-03 14:30:09 +0600

    Добавлена функция отображения данных на странице из firebase

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,40 @@
+import {useNavigate, useParams} from "react-router-dom";
+import axiosAPI from "../axios/AxiosAPI.tsx";
+import {useEffect, useState} from "react";
+interface Props{
+    title:string;
+    content:string;
+}
 const Home = () => {
+    const [pageData , setPageData] = useState<Props>()
+    const navigate = useNavigate();
+    const {pageName} = useParams();
+
+
+    useEffect(() => {
+        if(window.location.pathname === '/'){
+            axiosAPI.get(`/pages/home.json`)
+                .then(response => {
+                    setPageData(response.data)
+                });
+        }else if(window.location.pathname !== '/' && pageName !== 'admin'){
+            axiosAPI.get(`/pages/${pageName}.json`)
+                .then(response => {
+                    setPageData(response.data)
+                });
+        }
+    }, [pageName]);
+
     return (
-        <div>
-            123
+        <div className={'page-block'}>
+            {pageData ? (
+                <>
+                    <h1>{pageData.title}</h1>
+                    <p>{pageData.content}</p>
+                </>
+            ) : (
+                <p>Something gone wrong...</p>
+            )}
         </div>
     );
 };
